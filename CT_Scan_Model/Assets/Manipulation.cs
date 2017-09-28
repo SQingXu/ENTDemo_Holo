0d25c8a95623237b398f2cbb5c53ee48545a4fcc
Siqing Xu <xsq4525@cs.unc.edu>
2017-09-28 15:10:07 -0400

    initial commit with .gitignore

---
+++ CT_Scan_Model/Assets/Manipulation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Manipulation : MonoBehaviour {
+    float step = 0.05f;
+    Vector3 pos = new Vector3(0,0,0.5f);
+    bool Calibrate = true;
+
+    void Update()
+    {
+        if (Calibrate)
+        {
+            Vector3 cameraPos = Camera.main.transform.position;
+            Quaternion cameraRot = Camera.main.transform.rotation;
+            this.transform.position = cameraPos + cameraRot * pos;
+        }
+    }
+    void StopCalibration()
+    {
+        Calibrate = false;
+    }
+
+    void StartCalibration()
+    {
+        Calibrate = true;
+    }
+    void Further()
+    {
+        pos = new Vector3(pos.x, pos.y, pos.z + 0.1f);
+    }
+
+    void Nearer()
+    {
+        pos = new Vector3(pos.x, pos.y, pos.z - 0.1f);
+    }
+
+    
+	
+	
+}
---
+++ CT_Scan_Model/Assets/VoiceManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.Windows.Speech;
+
+
+public class VoiceManager : MonoBehaviour {
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+	// Use this for initialization
+	void Start () {
+        keywords.Add("Stay", () =>
+        {
+            this.BroadcastMessage("StopCalibration");
+            Debug.Log("Stay Recognized");
+        });
+        keywords.Add("Calibrate", () =>
+        {
+            this.BroadcastMessage("StartCalibration");
+            Debug.Log("Calibrate Recognized");
+        });
+        keywords.Add("Further", () =>
+        {
+            this.BroadcastMessage("Further");
+            Debug.Log("Further Recognized");
+        });
+        keywords.Add("Closer", () =>
+        {
+            this.BroadcastMessage("Nearer");
+            Debug.Log("Closer Recognized");
+        });
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs arg)
+    {
+        System.Action keywordAction;
+        if(keywords.TryGetValue(arg.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+
+    }
+}
